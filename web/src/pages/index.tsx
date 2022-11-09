ca19cf6c7ee43d62f8a6191728b73356b7bab315
kaiomaxlira <kaiomax01@gmail.com>
2022-11-09 19:12:31 -0300

    Iniciando estilizacao do front

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,22 +1,67 @@
-interface HomeProps {
+/* interface HomeProps {
   count: number;
-}
+} */
 
-export default function Home(props: HomeProps) {
+import Image from 'next/image'
+import appPreviewImg from '../assets/app-nlw-copa-preview.png'
+import logoImg from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
+import iconCheckImg from '../assets/icon-check.svg'
+
+export default function Home() {
   return (
-      <h1 className="text-violet-600 font-bold">Contagem: {props.count}</h1>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center">
+
+      <main>
+        <Image src={logoImg} alt="NLW Copa" /> 
+
+        <h1>Crie seu própio bolão de copa e compartilhe entre amigos!</h1>
+
+        <div>
+          <Image src={usersAvatarExampleImg} alt="" />
+          <strong>
+            <span>+12.592</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form>
+          <input type="text" required placeholder="Qual nome do seu bolão?" />
+          <button type="submit">Criar meu bolão</button>
+        </form>
+
+        <p> Após criar bolão, você receberá um código único que poderá usar para convidar outras pessoas </p>
+
+        <div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+            <span>+2.834</span>
+            <span>Bolões criados</span>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+            <span>+2.834</span>
+            <span>Bolões criados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+        <Image src={appPreviewImg} alt="celulares" quality={100} />
+        
+    </div>
   )
 }
 
-export const getServerSideProps = async () => {
+/* export const getServerSideProps = async () => {
   const response = await fetch('http://localhost:3333/pools/count')
   const data = await response.json()
-
-  console.log(data)
 
   return{
     props: {
       count: data.count,
     }
   }
-}
+} */
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '.src/**/*.tsx',
+    './src/**/*.tsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+
+      colors: {
+        gray: {
+          900: "#121214"
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
